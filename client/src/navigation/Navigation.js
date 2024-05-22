5cdd826bb584536e1832c9649ea6db788ce6aa90
DanMarBar <martinez.badan22@zaragoza.salesianos.edu>
2024-05-22 20:33:25 +0200

    Cambio de diseño de navbar y se añadio barra de busqueda

--- client/src/navigation/Navigation.js
+++ client/src/navigation/Navigation.js
@@ -1,33 +1,66 @@
 import * as React from 'react';
-import {useEffect, useState} from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import { useEffect, useState } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+
 import LoginScreen from '../screens/LoginScreen';
-import RegisterCreen from '../screens/RegisterScreen';
-import MainMenuScreen from "../screens/MainMenuScreen";
-import SignInScreen from "../screens/SignInScreen";
-import CarsListScreen from "../screens/cars/CarsListScreen";
-import CarDetailScreen from "../screens/cars/CarDetailScreen";
-import UpdateCarScreen from "../screens/cars/UpdateCarScreen";
-import InsertCarScreen from "../screens/cars/InsertCarScreen";
-import OnBoardingScreen from "../screens/OnBoardingScreen";
-import MenuModal from "../screens/modals/MenuModal";
-import RouteScreen from "../screens/RouteScreen";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import {TouchableOpacity, View} from "react-native";
-import MaterialIcons from "react-native-vector-icons/MaterialIcons";
-import UserUpdateScreen from "../screens/user/UserUpdateScreen";
-import InsertGroupScreen from "../screens/group/InsertGroupScreen";
-import ViewAllGroupsScreen from "../screens/group/ViewAllGroupsScreen";
-import ViewGroupDetailsScreen from "../screens/group/ViewGroupDetailsScreen";
-import PaymentScreen from "../screens/Payment";
-import GroupChatScreen from "../screens/group/GroupChatScreen";
-import UpdateGroupScreen from "../screens/group/UpdateGroupScreen";
+import RegisterScreen from '../screens/RegisterScreen';
+import MainMenuScreen from '../screens/MainMenuScreen';
+import SignInScreen from '../screens/SignInScreen';
+import CarsListScreen from '../screens/cars/CarsListScreen';
+import CarDetailScreen from '../screens/cars/CarDetailScreen';
+import UpdateCarScreen from '../screens/cars/UpdateCarScreen';
+import InsertCarScreen from '../screens/cars/InsertCarScreen';
+import OnBoardingScreen from '../screens/OnBoardingScreen';
+import MenuModal from '../screens/modals/MenuModal';
+import RouteScreen from '../screens/RouteScreen';
+import UserUpdateScreen from '../screens/user/UserUpdateScreen';
+import InsertGroupScreen from '../screens/group/InsertGroupScreen';
+import ViewAllGroupsScreen from '../screens/group/ViewAllGroupsScreen';
+import ViewGroupDetailsScreen from '../screens/group/ViewGroupDetailsScreen';
+import PaymentScreen from '../screens/Payment';
+import GroupChatScreen from '../screens/group/GroupChatScreen';
+import UpdateGroupScreen from '../screens/group/UpdateGroupScreen';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
     const [initialRoute, setInitialRoute] = useState('OnBoardingScreen');
     const [menuVisible, setMenuVisible] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredScreens, setFilteredScreens] = useState([]);
+    const navigation = useNavigation();
+
+    const screens = [
+        {name: 'OnBoardingScreen', component: OnBoardingScreen, options: {headerShown: false}},
+        {name: 'UserProfileScreen', component: UserUpdateScreen, options: {title: 'Perfil'}},
+        {name: 'Login', component: LoginScreen, options: {headerShown: false}},
+        {name: 'Register', component: RegisterScreen, options: {headerShown: false}},
+        {name: 'MainMenu', component: MainMenuScreen, options: {title: 'Menu principal'}},
+        {name: 'SignIn', component: SignInScreen},
+        {name: 'CarsListScreen', component: CarsListScreen, options: {title: 'Lista de Vehiculos'}},
+        {name: 'InsertCarScreen', component: InsertCarScreen, options: {title: 'Nuevo vehiculo'}},
+        {name: 'CarDetail', component: CarDetailScreen},
+        {name: 'UpdateCar', component: UpdateCarScreen},
+        {name: 'RouteScreen', component: RouteScreen},
+        {name: 'InsertGroupScreen', component: InsertGroupScreen, options: {title: 'Nuevo grupo'}},
+        {
+            name: 'ViewAllGroupsScreen',
+            component: ViewAllGroupsScreen,
+            options: {title: 'Ver Todos los Grupos'}
+        },
+        {
+            name: 'ViewGroupDetailsScreen',
+            component: ViewGroupDetailsScreen,
+            options: {title: 'Detalles del Grupo'}
+        },
+        {name: 'PaymentScreen', component: PaymentScreen},
+        {name: 'GroupChatScreen', component: GroupChatScreen},
+        {name: 'UpdateGroupScreen', component: UpdateGroupScreen},
+    ];
 
     useEffect(() => {
         const initialize = async () => {
@@ -51,16 +84,64 @@ const AppNavigator = () => {
         initialize();
     }, []);
 
+    useEffect(() => {
+        if (searchTerm === '') {
+            setFilteredScreens([]);
+        } else {
+            setFilteredScreens(
+                screens.filter(screen =>
+                    screen.options && screen.options.title && screen.options.title.toLowerCase().includes(searchTerm.toLowerCase())
+                )
+            );
+        }
+    }, [searchTerm]);
+
+    const handleSearchSelect = (screenName) => {
+        setSearchTerm('');
+        setFilteredScreens([]);
+        navigation.navigate(screenName);
+    };
+
     return (
         <>
             <Stack.Navigator
                 initialRouteName={initialRoute}
-                screenOptions={{
-                    headerStyle: {backgroundColor: '#39d9b3'},
-                    headerTintColor: '#050505',
-                    headerTitleStyle: {fontWeight: 'bold', color: '#000000'},
+                screenOptions={({navigation}) => ({
+                    headerStyle: {backgroundColor: '#131313'},
+                    headerTintColor: '#ffffff',
+                    headerTitleStyle: {fontWeight: 'bold', color: '#ffffff'},
+                    headerTitle: '', // This will hide the title in the navbar
+                    headerLeft: () => (
+                        <TouchableOpacity onPress={() => navigation.goBack()}>
+                            <MaterialIcons name="arrow-back" size={28} color="#ffffff"
+                                           style={{marginLeft: 10}}/>
+                        </TouchableOpacity>
+                    ),
                     headerRight: () => (
-                        <View style={{flexDirection: 'row', marginRight: 10}}>
+                        <View style={{flexDirection: 'row', marginRight: 10, alignItems: 'center'}}>
+                            <View>
+                                <TextInput
+                                    style={styles.searchInput}
+                                    placeholder="Search"
+                                    placeholderTextColor="#888"
+                                    value={searchTerm}
+                                    onChangeText={setSearchTerm}
+                                />
+                                {filteredScreens.length > 0 && (
+                                    <FlatList
+                                        data={filteredScreens}
+                                        renderItem={({item}) => (
+                                            <TouchableOpacity
+                                                onPress={() => handleSearchSelect(item.name)}>
+                                                <Text
+                                                    style={styles.searchResult}>{item.options.title}</Text>
+                                            </TouchableOpacity>
+                                        )}
+                                        keyExtractor={item => item.name}
+                                        style={styles.searchResultsContainer}
+                                    />
+                                )}
+                            </View>
                             <TouchableOpacity onPress={() => navigation.navigate('MainMenu')}>
                                 <MaterialIcons name="home" size={28} color="#ffffff"
                                                style={{marginRight: 20}}/>
@@ -70,33 +151,51 @@ const AppNavigator = () => {
                             </TouchableOpacity>
                         </View>
                     ),
-                }}
+                })}
             >
-                <Stack.Screen name="OnBoardingScreen" component={OnBoardingScreen}/>
-                <Stack.Screen name="UserProfileScreen" component={UserUpdateScreen}/>
-                <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}/>
-                <Stack.Screen name="Register" component={RegisterCreen}
-                              options={{headerShown: false}}/>
-                <Stack.Screen name="MainMenu" component={MainMenuScreen} options={{
-                    title: "Tu" +
-                        " menu principal"
-                }}/>
-                <Stack.Screen name="SignIn" component={SignInScreen}/>
-                <Stack.Screen name="CarsListScreen" component={CarsListScreen}/>
-                <Stack.Screen name="InsertCarScreen" component={InsertCarScreen}/>
-                <Stack.Screen name="CarDetail" component={CarDetailScreen}/>
-                <Stack.Screen name="UpdateCar" component={UpdateCarScreen}/>
-                <Stack.Screen name="RouteScreen" component={RouteScreen}/>
-                <Stack.Screen name="InsertGroupScreen" component={InsertGroupScreen}/>
-                <Stack.Screen name="ViewAllGroupsScreen" component={ViewAllGroupsScreen}/>
-                <Stack.Screen name="ViewGroupDetailsScreen" component={ViewGroupDetailsScreen}/>
-                <Stack.Screen name="PaymentScreen" component={PaymentScreen}/>
-                <Stack.Screen name="GroupChatScreen" component={GroupChatScreen}/>
-                <Stack.Screen name="UpdateGroupScreen" component={UpdateGroupScreen}/>
+                {screens.map(screen => (
+                    <Stack.Screen
+                        key={screen.name}
+                        name={screen.name}
+                        component={screen.component}
+                        options={screen.options}
+                    />
+                ))}
             </Stack.Navigator>
             <MenuModal isVisible={menuVisible} onClose={() => setMenuVisible(false)}/>
         </>
     );
 };
+
+const styles = StyleSheet.create({
+    searchInput: {
+        borderWidth: 1,
+        borderColor: '#888',
+        backgroundColor: '#333',
+        color: '#fff',
+        padding: 8,
+        width: 240,
+        marginRight: 10,
+        borderRadius: 20,
+    },
+    searchResultsContainer: {
+        position: 'absolute',
+        top: 50,
+        left: 0,
+        right: 0,
+        backgroundColor: '#1c1c1c',
+        borderWidth: 1,
+        borderColor: '#444',
+        zIndex: 1,
+        borderRadius: 20,
+    },
+    searchResult: {
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#444',
+        color: '#fff',
+        borderRadius: 20,
+    },
+});
 
 export default AppNavigator;
